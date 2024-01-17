a2f07cf46b20ebd166e4c5ebb67f254ed647ccb4
Alena Dvorakova <alenadvorakova@technicky-preklad.cz>
2024-01-17 17:15:25 +0100

    Rybka plave okolo kamene, pocetKroku vyřešen proměnnou.

--- src/main/java/cz/czechitas/java/lekce01/RybkaApplication.java
+++ src/main/java/cz/czechitas/java/lekce01/RybkaApplication.java
@@ -34,16 +34,17 @@ public class RybkaApplication extends CzechitasGameApplication {
   protected void onStart() {
     kamen = new Kamen(200, 200);
     rybka = new Rybka(100, 100);
-    for(int i = 0; i < 20; i++) {
+    int pocetKroku = 20;
+    for(int i = 0; i < pocetKroku; i++) {
       rybka.plavatVpravo();
     }
-    for(int i = 0; i < 20; i++) {
+    for(int i = 0; i < pocetKroku; i++) {
       rybka.plavatDolu();
     }
-    for(int i = 0; i < 20; i++) {
+    for(int i = 0; i < pocetKroku; i++) {
       rybka.plavatVlevo();
     }
-    for(int i = 0; i < 20; i++) {
+    for(int i = 0; i < pocetKroku; i++) {
       rybka.plavatNahoru();
     }
   }
